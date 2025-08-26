fb1d3cfe2135a72629f21e7b95c15d4134335d94
Hlib Kaponkin <glebkapenkin@gmail.com>
2025-08-26 18:23:02 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { createServer } from "./createServer.js";
 import 'dotenv/config';
 
-const {POSTGRES_PORT} = process.env;
+const { POSTGRES_PORT } = process.env;
 
 createServer().listen(POSTGRES_PORT, () => { })
