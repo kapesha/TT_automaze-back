e0e5fb1d60380009dfbe87d1277e8869484b9784
Hlib Kaponkin <glebkapenkin@gmail.com>
2025-08-26 17:33:20 +0300

    fixes

--- src/db.js
+++ src/db.js
@@ -6,6 +6,7 @@ const {
   POSTGRES_USER,
   POSTGRES_PASSWORD,
   POSTGRES_DB,
+  POSTGRES_PORT,
 } = process.env;
 
 export const sequelize = new Sequelize(POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, {
